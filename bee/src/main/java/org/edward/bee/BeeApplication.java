4676f11cfbcd166187d1f5ad783b539469c0ad82
Edward_89 <wangwei.891126@gmail.com>
2024-01-20 10:16:52 +0800

    添加SpringBoot依赖

---
+++ bee/src/main/java/org/edward/bee/BeeApplication.java
@@ -0,0 +1,11 @@
+package org.edward.bee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BeeApplication.class, args);
+    }
+}
