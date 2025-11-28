770f3ba039b273f06d79d9721e69cd080d25f517
Edward_89 <wangwei.891126@gmail.com>
2025-11-28 17:03:17 +0800

    添加Http加解密工具类

---
+++ common/src/main/java/org/edward/pandora/common/codec/HttpRequest.java
@@ -0,0 +1,13 @@
+package org.edward.pandora.common.codec;
+
+public class HttpRequest {
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+    public HttpRequest setData(String data) {
+        this.data = data;
+        return this;
+    }
+}
